4498cfb724d52333fe4e429d084f1a6e021ff9cf
Venelin Efremov <ghent360@iqury.us>
2022-11-24 09:19:43 -0800

    Add comments to the example Arduino code.

--- OdriveCanTest.ino
+++ OdriveCanTest.ino
@@ -4,46 +4,19 @@
 
 using odrive::CanMsgData;
 using odrive::ODriveAxis;
+using odrive::ParseCanMsg;
 using odrive::VbusVoltage;
 
 SAME51_CAN can;
 
+// Function that interfaces ODrive with our can hardware. If you want to
+// use multiple CAN buses you would need one for each bus.
 static void SendCmdCh0(uint32_t canId, uint8_t len, uint8_t *buf) {
   can.sendMsgBuf(canId, 0, len, buf);
 }
 
-ODriveAxis axesCh0[] = {
-  ODriveAxis(1, SendCmdCh0),
-  ODriveAxis(3, SendCmdCh0),
-  ODriveAxis(5, SendCmdCh0),
-  ODriveAxis(7, SendCmdCh0),
-  ODriveAxis(9, SendCmdCh0),
-  ODriveAxis(11, SendCmdCh0),
-};
-
-PeriodicTimer myTimers[] = {
-  PeriodicTimer(150, [](uint32_t)->void {
-    for(auto& axis : axesCh0) {
-      axis.hb.PeriodicCheck(axis);
-      if (axis.hb.error != 0) {
-        Serial.print("Error axis ");
-        Serial.print(axis.node_id);
-        Serial.print(" code ");
-        Serial.println(axis.hb.error);
-      }
-    }
-  }),
-  PeriodicTimer(1000, [](uint32_t)->void {
-    static uint8_t axis = 0;
-    if (axesCh0[axis].hb.alive) {
-      axesCh0[axis].RequestVbusVoltage();
-    }
-    axis++;
-    if (axis > 5) axis = 0;
-  })
-};
-
-void PrintCanMessage(uint32_t id, uint8_t len, const CanMsgData& buf) {
+// Helper to print CAN messages we can not parse.
+static void PrintCanMessage(uint32_t id, uint8_t len, const CanMsgData& buf) {
   Serial.print("Id: ");
   Serial.print(id);
   Serial.print(" Len: ");
@@ -56,14 +29,50 @@ void PrintCanMessage(uint32_t id, uint8_t len, const CanMsgData& buf) {
   Serial.println();
 }
 
+// We communicate with 3 ODrive boards, each board has 2 axes. Each axis
+// has to be setup separately. If you have boards that are connected to a
+// different CAN bus, you would need separate callback for each bus. For
+// example SendCmdCh1.
+//
+// It is requered to have unique node_id even if some axis are on a
+// separate CAN bus. Otherwise the parsing code has to be modified to 
+// process each CAN bus separately.
+ODriveAxis axesCh0[] = {
+  ODriveAxis(1, SendCmdCh0),
+  ODriveAxis(3, SendCmdCh0),
+  ODriveAxis(5, SendCmdCh0),
+  ODriveAxis(7, SendCmdCh0),
+  ODriveAxis(9, SendCmdCh0),
+  ODriveAxis(11, SendCmdCh0),
+};
+
+PeriodicTimer myTimers[] = {
+  // Check that hearthbeat message was received every 150ms.
+  PeriodicTimer(150, [](uint32_t)->void {
+    for(auto& axis : axesCh0) {
+      axis.hb.PeriodicCheck(axis);
+    }
+  }),
+  // Request each axis vbus voltage every second and cycle trough
+  // the axes. Only request vbus voltage is the axis is alive.
+  PeriodicTimer(1000, [](uint32_t)->void {
+    static uint8_t axis = 0;
+    if (axesCh0[axis].hb.alive) {
+      axesCh0[axis].RequestVbusVoltage();
+    }
+    axis++;
+    if (axis > 5) axis = 0;
+  })
+};
+
+// Helper to parse incomming CAN messages or print if parsing was
+// nos successful.
 void ProcessCanMessage(uint32_t id, uint8_t len, const CanMsgData& buf) {
   bool parseSuccess = ParseCanMsg(axesCh0, id, len, buf);
   if (!parseSuccess) {
     PrintCanMessage(id, len, buf);
   }
 }
-
-uint32_t startTime = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -77,6 +86,8 @@ void setup() {
       Serial.println("Error Initializing CAN...");
       while(1);
   }
+  // Setup callbcaks, when asix becomes unavailable or
+  // when we receive the vbus voltage data.
   for(auto& axis : axesCh0) {
     axis.vbus.SetCallback([](ODriveAxis& axis, VbusVoltage& v) { 
       Serial.print("Axis ");
@@ -90,20 +101,18 @@ void setup() {
       Serial.println(" unreachable");
     });
   }
-  startTime = millis();
-  StartAllTimers(myTimers, startTime);
+  StartAllTimers(myTimers, millis());
 }
 
 void loop() {
-  uint8_t ret;
   uint32_t id;
   uint8_t len;
   uint8_t buf[8];
-  uint8_t i;
 
+  // process periodic timers
   CheckAllTimers(myTimers, millis());
-  ret = can.readMsgBuf(&id, &len, buf);
-  if (ret == CAN_OK) {
+  // check for CAN messages and precess them.
+  if (can.readMsgBuf(&id, &len, buf) == CAN_OK) {
     ProcessCanMessage(id, len, buf);
   }
 }

1a9d52b0a9387c864a3c3e37f56f7c0691b31c54
Venelin Efremov <ghent360@iqury.us>
2022-12-24 02:42:05 -0800

    Remove the old PeriodicTimer class, it is superseeded by the TaskManager class.

--- CanInterface.h
+++ CanInterface.h
@@ -20,6 +20,9 @@ public:
     // Adapt these to match your board configuration. I use 2 CAN busses
     // CAN1 and CAN3 - each of these methods sends a message to the 
     // corresponding CAN 'channel'.
+    //
+    // It is ok to leave one unused, but if you need more can busses,
+    // add more of these static methods.
     static void sendCmdCh1(uint32_t canId, uint8_t len, uint8_t *buf);
     static void sendCmdCh3(uint32_t canId, uint8_t len, uint8_t *buf);
 private:

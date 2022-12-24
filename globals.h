4fb285bb66a87ae0dd15fc30651b655609d4ebe9
Venelin Efremov <ghent360@iqury.us>
2022-12-24 02:48:53 -0800

    Add some comments to the globals.h file.

--- globals.h
+++ globals.h
@@ -6,11 +6,23 @@
 #include "ODriveCan.hpp"
 #include "CanInterface.h"
 
+// various global data structures used by several files
+
+// My robot uses 12 axes.
 constexpr int numAxes = 12;
+
+// Static offset for each joint at its resting position.
 extern const float jointOffsets[numAxes]; // Index with DogLegJoint value
+
+// An AxisClass for each of the axes.
 extern const AxisClass jointClass[numAxes]; // Index with DogLegJoint value
-extern odrive::ODriveAxis axes[numAxes]; // Index with DogLegJoint value (defined in CanInterface.cpp)
+// The actual ODriveAxis object for each motor axis.
+extern odrive::ODriveAxis axes[numAxes]; // Index with DogLegJoint value
+// A name for each axis.
 extern const char* axisName[numAxes]; // Index with DogLegJoint value
 
+// A name for each leg.
 const char* getLegName(DogLeg leg);
+
+// Our CanInterface instance.
 extern odrive::CanInterface canInterface;

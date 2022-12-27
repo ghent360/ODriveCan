6e2193240137aba8598e2785afe486c3f18e6a0c
Venelin Efremov <ghent360@iqury.us>
2022-12-27 03:06:44 -0800

    Refactro the display code. Add widget classes.

--- Display.cpp
+++ Display.cpp
@@ -6,8 +6,6 @@
 #include "Display.h"
 #include "globals.h"
 
-#include <stdint.h>
-#include <ST7735_t3.h> // Hardware-specific library
 #include <st7735_t3_font_Arial.h>
 
 // Screen pin definitions
@@ -22,6 +20,14 @@
 static ST7735_t3 tft = ST7735_t3(
   TFT_CS, TFT_DC, TFT_MOSI, TFT_SCK, TFT_RST);
 
+Display::Display() 
+  : teensy_battery_("M:", 0, 5, 2),
+    bus1_battery_("1: ", 0, 5 + (BatteryWidget::batteryBarHeight + 1), 6),
+    bus3_battery_("2: ", 0, 5 + 2*(BatteryWidget::batteryBarHeight + 1), 6),
+    can_status_(
+      0, 5 + 4*(BatteryWidget::batteryBarHeight + 1), 8, ST7735_WHITE) {
+}
+
 void Display::initDisplay() {
   pinMode(TFT_LED, OUTPUT);
   digitalWrite(TFT_LED, 1);
@@ -29,7 +35,6 @@ void Display::initDisplay() {
   tft.setRotation(3);
   tft.useFrameBuffer(true);
   tft.setFont(Arial_8);
-  dirty_ = true;
 }
 
 void Display::stopDisplay() {
@@ -37,80 +42,99 @@ void Display::stopDisplay() {
 }
 
 void Display::updateScreen() {
-  if (dirty_) {
+  if (dirty()) {
     tft.waitUpdateAsyncComplete();
     drawUi();
     tft.updateScreenAsync();
   }
 }
 
-constexpr uint8_t batteryBarWidth = 30;
-constexpr uint8_t batteryBarHeight = 8;
-constexpr uint16_t batteryBarColor = ST7735_WHITE;
+void Display::drawUi() {
+  for(auto widget : widgets_) {
+    widget->draw();
+  }
+}
 
-static void drawBatteryVoltage(
-    uint8_t x, uint8_t y,
-    const char* label,
-    float voltage,
-    uint8_t nCells
-    ) {
-  const float maxVoltage = cellMaxVoltage * nCells;
-  const float warnVoltage = cellWarnVoltage * nCells;
-  const float lowVoltage = ((cellWarnVoltage + cellMinVoltage) / 2) * nCells;
-  const float minVoltage = cellMinVoltage * nCells;
+BatteryWidget::BatteryWidget(
+  const char* label, uint8_t x, uint8_t y, uint8_t numCells)
+  : Widget(x, y), label_(label), num_cells_(numCells) {
+  if (label_) {
+    int16_t x1, y1;
+    uint16_t labelW, labelH;
+    tft.setTextSize(6);
+    tft.getTextBounds(label_, x_, y_, &x1, &y1, &labelW, &labelH);
+    bar_x_offset_ = labelW + 2;
+    w_ += bar_x_offset_;
+    if (labelH > h_) {
+      h_ = labelH;
+      bar_y_offset_ = (h_ - batteryBarHeight) / 2;
+    } else {
+      label_y_offset_ = (h_ - labelH) / 2;
+    }
+  }
+}
+
+void BatteryWidget::draw() {
+  if (!dirty_) return;
+
+  // Clear the widget area
+  tft.fillRect(x_, y_, w_, h_, ST7735_BLACK);
+
+  const float maxVoltage = cellMaxVoltage * num_cells_;
+  const float warnVoltage = ((cellMaxVoltage + cellWarnVoltage) / 2) * num_cells_;
+  const float lowVoltage = ((cellWarnVoltage + cellMinVoltage) / 2) * num_cells_;
+  const float minVoltage = cellMinVoltage * num_cells_;
   uint16_t color;
-  int16_t x1, y1;
-  uint16_t labelW, labelH;
-  uint8_t barHeight = batteryBarHeight;
-  if (voltage > maxVoltage || voltage < lowVoltage) {
+  if (voltage_ > maxVoltage || voltage_ < lowVoltage) {
     color = ST7735_RED;
-  } else if (voltage < warnVoltage) {
+  } else if (voltage_ < warnVoltage) {
     color = ST7735_YELLOW;
   } else {
     color = ST7735_GREEN;
   }
 
+  float voltage = voltage_;
   if (voltage > maxVoltage) {
     voltage = maxVoltage;
   }
 
-  if (label) {
+  if (label_) {
     tft.setTextColor(color);
     tft.setTextSize(6);
-    tft.drawString(label, x, y);
-    tft.getTextBounds(label, x, y, &x1, &y1, &labelW, &labelH);
-    x += labelW + 2;
-    if (labelH > barHeight) {
-        barHeight = labelH;
-    }
+    tft.drawString(label_, x_, label_y_offset_);
   }
+  uint8_t x = x_ + bar_x_offset_;
+  uint8_t y = y_ + bar_y_offset_;
   tft.drawFastHLine(x, y, batteryBarWidth, batteryBarColor);
-  tft.drawFastHLine(x, y + barHeight - 1, batteryBarWidth, batteryBarColor);
-  tft.drawFastVLine(x, y, barHeight, batteryBarColor);
-  tft.drawFastVLine(x + batteryBarWidth - 1, y, barHeight, batteryBarColor);
+  tft.drawFastHLine(x, y + batteryBarHeight - 1, batteryBarWidth, batteryBarColor);
+  tft.drawFastVLine(x, y, batteryBarHeight, batteryBarColor);
+  tft.drawFastVLine(x + batteryBarWidth - 1, y, batteryBarHeight, batteryBarColor);
   uint16_t barWidth = 
     (uint16_t)(
         (float)(batteryBarWidth - 2) * (voltage - minVoltage) / 
         (maxVoltage - minVoltage) + 
         0.5f);
-  tft.fillRect(x + 1, y + 1, barWidth, barHeight - 2, color);
-}
-
-void Display::drawCanStatus() {
-  if (can_status_) {
-    tft.setTextColor(ST7735_WHITE);
-    tft.setTextSize(8);
-    tft.drawString(can_status_, 0, 5 + 4*(batteryBarHeight + 1));
-  }
-}
-
-void Display::drawUi() {
-  tft.fillScreen(ST7735_BLACK);
-  drawBatteryVoltage(0, 5, "M:", teensy_battery_voltage_, 2);
-  drawBatteryVoltage(0, 5 + (batteryBarHeight + 1), "1: ", bus1_battery_voltage_, 6);
-  drawBatteryVoltage(0, 5 + 2*(batteryBarHeight + 1), "2: ", bus3_battery_voltage_, 6);
-  drawCanStatus();
+  tft.fillRect(x + 1, y + 1, barWidth, batteryBarHeight - 2, color);
   dirty_ = false;
+}
+
+void StatusWidget::draw() {
+  if (!dirty_) return;
+  // Clear the old widget area
+  tft.fillRect(x_, y_, old_w_, old_h_, ST7735_BLACK);
+
+  if (status_) {
+    tft.setTextColor(color_);
+    tft.setTextSize(font_size_);
+    tft.drawString(status_, x_, y_);
+  }
+  dirty_ = false;
+}
+
+void StatusWidget::getSize(uint16_t &w, uint16_t &h) {
+  int16_t x1, y1;
+  tft.setTextSize(font_size_);
+  tft.getTextBounds(status_, x_, y_, &x1, &y1, &w, &h);
 }
 
 Display display;
--- Display.h
+++ Display.h
@@ -5,47 +5,118 @@
 #pragma once
 
 #include <math.h>
+#include <stdint.h>
+#include <ST7735_t3.h> // Hardware-specific library
+
+class Widget {
+public:
+  Widget(uint8_t x, uint8_t y)
+    : x_(x), y_(y), dirty_(false) {}
+  virtual ~Widget() {}
+
+  bool dirty() const { return dirty_; }
+  virtual void draw() = 0;
+  virtual void getSize(uint16_t &w, uint16_t &h) = 0;
+protected:
+  const uint8_t x_;
+  const uint8_t y_;
+  bool dirty_;
+};
+
+class BatteryWidget: public Widget {
+public:
+  static constexpr uint8_t batteryBarWidth = 30;
+  static constexpr uint8_t batteryBarHeight = 8;
+
+  BatteryWidget(const char* label, uint8_t x, uint8_t y, uint8_t numCells);
+
+  void setVoltage(float v) {
+    if (fabsf(v - voltage_) > 0.01) {
+      voltage_ = v;
+      dirty_ = true;
+    }
+  }
+
+  void draw() override;
+  void getSize(uint16_t &w, uint16_t &h) override {
+    w = w_;
+    h = h_;
+  }
+private:
+  static constexpr uint16_t batteryBarColor = ST7735_WHITE;
+
+  uint16_t w_ = batteryBarWidth;
+  uint16_t h_ = batteryBarHeight;
+  const char *label_;
+  const uint8_t num_cells_;
+  float voltage_;
+  uint8_t label_y_offset_ = 0;
+  uint8_t bar_y_offset_ = 0;
+  uint8_t bar_x_offset_ = 0;
+};
+
+class StatusWidget: public Widget {
+public:
+  StatusWidget(uint8_t x, uint8_t y, uint8_t fontSize, uint16_t color)
+    : Widget(x, y), status_(nullptr), font_size_(fontSize), color_(color) {}
+
+  void setStatus(const char* status) {
+    getSize(old_w_, old_h_);
+    status_ = status;
+    dirty_ = true;
+  }
+
+  void draw() override;
+  void getSize(uint16_t &w, uint16_t &h) override;
+private:
+  const char* status_;
+  const uint8_t font_size_;
+  const uint16_t color_;
+  uint16_t old_w_;
+  uint16_t old_h_;
+};
 
 class Display {
 public:
+  Display();
+
   void initDisplay();
   void stopDisplay();
   void updateScreen();
 
   void setTeensyBatteryVoltage(float v) {
-    if (fabsf(v - teensy_battery_voltage_) > 0.01) {
-      teensy_battery_voltage_ = v;
-      dirty_ = true;
-    }
+    teensy_battery_.setVoltage(v);
   }
 
   void setBus1BatteryVoltage(float v) {
-    if (fabsf(v - bus1_battery_voltage_) > 0.01) {
-      bus1_battery_voltage_ = v;
-      dirty_ = true;
-    }
+    bus1_battery_.setVoltage(v);
   }
 
   void setBus3BatteryVoltage(float v) {
-    if (fabsf(v - bus3_battery_voltage_) > 0.01) {
-      bus3_battery_voltage_ = v;
-      dirty_ = true;
-    }
+    bus3_battery_.setVoltage(v);
   }
 
   void setCanStatus(const char* msg) {
-    if (can_status_ != msg) {
-      can_status_ = msg;
-      dirty_ = true;
-    }
+    can_status_.setStatus(msg);
   }
 private:
   void drawUi();
-  void drawCanStatus();
-  
-  float teensy_battery_voltage_;
-  float bus1_battery_voltage_;
-  float bus3_battery_voltage_;
-  const char* can_status_;
-  bool dirty_;
+  bool dirty() const {
+    for(auto widget : widgets_) {
+      if (widget->dirty()) return true;
+    }
+    return false;
+  }
+
+  BatteryWidget teensy_battery_;
+  BatteryWidget bus1_battery_;
+  BatteryWidget bus3_battery_;
+  StatusWidget can_status_;
+
+  Widget* widgets_[4] = {
+    &teensy_battery_,
+    &bus1_battery_,
+    &bus3_battery_,
+    &can_status_
+  };
 };

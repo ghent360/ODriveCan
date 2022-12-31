47f948de0868384dda7013cc08b3dfc9467a50af
Venelin Efremov <ghent360@iqury.us>
2022-12-31 00:26:20 -0800

    Add BitstreamWriter helper class.

--- Bitstream.hpp
+++ Bitstream.hpp
@@ -89,3 +89,133 @@ private:
   uint8_t data_left_;
   bool error_;
 };
+
+class BitstreamWriter {
+public:
+  BitstreamWriter(uint8_t* data, uint8_t len)
+    : curr_(0),
+      curr_bits_left_(32),
+      data_(data),
+      data_left_(len),
+      error_(false) {}
+
+  void flush() {
+    store();
+  }
+
+  void writeBit(uint8_t v) {
+    if (!curr_bits_left_) store();
+    curr_ |= ((uint32_t)(v & 1) << (curr_bits_left_ - 1));
+    curr_bits_left_--;
+  }
+
+  void writeBits(uint32_t v, uint8_t n) {
+    if (!n || n > 32) return;
+    v &= masks_[n];
+    if (curr_bits_left_ >= n) {
+      curr_ |= (v << (curr_bits_left_ - n));
+      curr_bits_left_ -= n;
+    } else {
+      uint8_t rem = n - curr_bits_left_;
+      curr_ |= (v >> rem);
+      curr_bits_left_ = 0;
+      store();
+      v &= masks_[rem];
+      curr_ |= (v << (curr_bits_left_ - rem));
+      curr_bits_left_ -= rem;
+    }
+  }
+
+  bool error() const {
+    return error_;
+  }
+private:
+  static constexpr uint32_t masks_[33] = {
+    0,
+    0x00000001,
+    0x00000003,
+    0x00000007,
+    0x0000000F,
+    0x0000001F,
+    0x0000003F,
+    0x0000007F,
+    0x000000FF,
+    0x000001FF,
+    0x000003FF,
+    0x000007FF,
+    0x00000FFF,
+    0x00001FFF,
+    0x00003FFF,
+    0x00007FFF,
+    0x0000FFFF,
+    0x0001FFFF,
+    0x0003FFFF,
+    0x0007FFFF,
+    0x000FFFFF,
+    0x001FFFFF,
+    0x003FFFFF,
+    0x007FFFFF,
+    0x00FFFFFF,
+    0x01FFFFFF,
+    0x03FFFFFF,
+    0x07FFFFFF,
+    0x0FFFFFFF,
+    0x1FFFFFFF,
+    0x3FFFFFFF,
+    0x7FFFFFFF,
+    0xFFFFFFFF,
+  };
+
+  void store() {
+    uint8_t old_bits_left = curr_bits_left_;
+    curr_bits_left_ = 32;
+    if (data_left_ >= 4) {
+      data_[0] = curr_ >> 24;
+      data_[1] = curr_ >> 16;
+      data_[2] = curr_ >> 8;
+      data_[3] = curr_;
+      curr_ = 0;
+      data_ += 4;
+      data_left_ -= 4;
+      return;
+    }
+    if (data_left_ == 3) {
+      data_[0] = curr_ >> 24;
+      data_[1] = curr_ >> 16;
+      data_[2] = curr_ >> 8;
+      curr_ = 0;
+      data_ += 3;
+      data_left_ = 0;
+      if (old_bits_left < 8)
+        error_ = true;
+      return;
+    }
+    if (data_left_ == 2) {
+      data_[0] = curr_ >> 24;
+      data_[1] = curr_ >> 16;
+      curr_ = 0;
+      data_ += 2;
+      data_left_ = 0;
+      if (old_bits_left < 16)
+        error_ = true;
+      return;
+    }
+    if (data_left_ == 1) {
+      data_[0] = curr_ >> 24;
+      curr_ = 0;
+      data_ += 1;
+      data_left_ = 0;
+      if (old_bits_left < 24)
+        error_ = true;
+      return;
+    }
+    curr_ = 0;
+    error_ = true;
+  }
+
+  uint32_t curr_;
+  uint8_t curr_bits_left_;
+  uint8_t* data_;
+  uint8_t data_left_;
+  bool error_;
+};
--- tests/bitstreamTest.cpp
+++ tests/bitstreamTest.cpp
@@ -74,3 +74,101 @@ TEST_SUITE("BitstreamReader tests")
         CHECK(br1.error() == false);
     }
 }
+
+TEST_SUITE("BitstreamWriter tests")
+{
+    TEST_CASE("write bit")
+    {
+        uint8_t data[4];
+        BitstreamWriter bw(data, sizeof(data));
+
+        CHECK(bw.error() == false);
+        bw.writeBit(1);
+        CHECK(bw.error() == false);
+        bw.writeBit(0);
+        bw.writeBit(1);
+        bw.writeBit(0);
+        bw.writeBit(0);
+        bw.writeBit(1);
+        bw.writeBit(0);
+        bw.writeBit(1);
+        for(int i = 0; i < 24; i++) {
+            bw.writeBit(0);
+            CHECK(bw.error() == false);
+        }
+        bw.flush();
+        CHECK(bw.error() == false);
+        CHECK(data[0] == 0xa5);
+        CHECK(data[1] == 0);
+        CHECK(data[2] == 0);
+        CHECK(data[3] == 0);
+   }
+
+   TEST_CASE("write bit 32")
+   {
+        uint8_t data[5];
+        BitstreamWriter bw(data, sizeof(data));
+
+        bw.writeBits(0xa5000000, 32);
+        CHECK(bw.error() == false);
+        bw.writeBits(0x1a, 5);
+        CHECK(bw.error() == false);
+        bw.flush();
+        CHECK(bw.error() == false);
+        CHECK(data[0] == 0xa5);
+        CHECK(data[1] == 0);
+        CHECK(data[2] == 0);
+        CHECK(data[3] == 0);
+        CHECK(data[4] == 0xd0); // 0b11010000
+
+        uint8_t data1[5];
+        BitstreamWriter bw2(data1, sizeof(data1));
+        bw2.writeBits(0x14, 5); // 0b10100
+        bw2.writeBits(0xa0000005, 32);
+        bw2.flush();
+        CHECK(bw2.error() == false);
+        CHECK(data1[0] == 0xa5);
+        CHECK(data1[1] == 0);
+        CHECK(data1[2] == 0);
+        CHECK(data1[3] == 0);
+        CHECK(data1[4] == 0x28); // 0b0101000
+   }
+
+   TEST_CASE("write bits n")
+   {
+        uint8_t data[4];
+        BitstreamWriter bw(data, sizeof(data));
+        bw.writeBits(0xa5, 8);
+        bw.writeBits(0x0f, 4);
+        bw.writeBits(0x02, 6); // 0b000010
+        bw.writeBits(0x0c, 6); // 0b001100
+        bw.writeBits(0x01, 5); // 0b00001
+        bw.writeBits(0x07, 3); // 0b00111
+        bw.flush();
+        CHECK(bw.error() == false);
+        CHECK(data[0] == 0xa5);
+        CHECK(data[1] == 0xf0);
+        CHECK(data[2] == 0x8c);
+        CHECK(data[3] == 0x0f);
+   }
+
+    TEST_CASE("write bits cross")
+    {
+        uint8_t data[8];// = {0xa5, 0xf0, 0x8c, 0x0f, 0x75, 0x36};
+        BitstreamWriter bw(data, sizeof(data));
+
+        bw.writeBits(0x0a5f08, 20);
+        bw.writeBits(0x0c0f75, 20);
+        bw.writeBits(0x036001, 20);
+        bw.flush();
+        CHECK(bw.error() == false);
+        CHECK(data[0] == 0xa5);
+        CHECK(data[1] == 0xf0);
+        CHECK(data[2] == 0x8c);
+        CHECK(data[3] == 0x0f);
+        CHECK(data[4] == 0x75);
+        CHECK(data[5] == 0x36);
+        CHECK(data[6] == 0);
+        CHECK(data[7] == 0x10); // 0b00010000
+    }
+}
